eb3c9a604790e443edac9b5210db701d3d0129b1
korkmazkadir <korkmazkadir@gmail.com>
2022-07-17 20:29:07 +0200

    Updates report, and implements a new stats aggregator to collect stats from zip files

--- analytics/stats-aggregator.R
+++ analytics/stats-aggregator.R
@@ -58,31 +58,43 @@ create_message_received_df <- function(config, stats_df) {
 }
 
 
-create_queue_length_df <- function(config, stats_df) {
+create_network_usage_df <- function(config, stats_df) {
   result_df <- as.data.frame(config)
-  df <- stats_df %>% filter(Event == "QUEUE_LENGTH")
-
+  df <- stats_df %>% filter(Event == "NETWORK_USAGE")
+  
+  # TODO: is it a good way to solve the problem
+  # int overflow problem 
+  df$Value <- as.numeric(df$Value)
+  
+  typeof(df$Value)
+  
+  stats <- boxplot.stats(df$Value)$stats
+  
   mean <- mean(df$Value)
   ci <- confidence_interval(df)
-
-  result_df["LowerBound"] <- mean - ci
-  result_df["Mean"] <- mean
-  result_df["UpperBound"] <- mean + ci
+  
+  result_df["Min"] <- c(stats[1])
+  result_df["FirstQuartile"] <- c(stats[2])
+  result_df["Median"] <- c(stats[3])
+  result_df["ThirdQuartile"] <- c(stats[4])
+  result_df["Max"] <- c(stats[5])
   result_df["RowCount"] <- c( nrow(df) )
-
+  result_df["MeanLowerBound"] <- mean - ci
+  result_df["Mean"] <- mean
+  result_df["MeanUpperBound"] <- mean + ci
+  
   return(result_df)
 }
 
 
 # iterates over directories
-
-first_chunk_delivery_df <- data.frame(matrix(ncol = 13, nrow = 0))
-message_received_df <- data.frame(matrix(ncol = 11, nrow = 0))
-queue_length_df <- data.frame(matrix(ncol = 11, nrow = 0))
-
 calculate_datasets <- function(directories) {
   print("Processing...")
 
+  first_chunk_delivery_df <- data.frame(matrix(ncol = 13, nrow = 0))
+  message_received_df <- data.frame(matrix(ncol = 11, nrow = 0))
+  network_usage_df <- data.frame(matrix(ncol = 11, nrow = 0))
+  
   for (directory in directories) {
     config_file <- paste(directory, "/config.json", sep = "")
     if (file.exists(config_file) == FALSE) {
@@ -106,20 +118,20 @@ calculate_datasets <- function(directories) {
     message_received_df <- rbind(message_received_df, df)
 
     # queue size
-    df <- create_queue_length_df(config, stats)
-    queue_length_df <- rbind(queue_length_df, df)
+    df <- create_network_usage_df(config, stats)
+    network_usage_df <- rbind(network_usage_df, df)
   }
 
   # write data frames
   write.table(first_chunk_delivery_df, file = paste(path, "/first_chunk_delivery_df.tsv", sep = ""), quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
   write.table(message_received_df, file = paste(path, "/message_received_df.tsv", sep = ""), quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
-  write.table(queue_length_df, file = paste(path, "/queue_length_df.tsv", sep = ""), quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
+  write.table(network_usage_df, file = paste(path, "/network_usage_df.tsv", sep = ""), quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
 }
 
-path <- "/home/kadir/Desktop/ida_connection_count_effect"
+#path <- "/home/kadir/Desktop/ida_connection_count_effect"
 
 # lists directories
-directories <- list.dirs(path = path, full.names = TRUE, recursive = TRUE)
+#directories <- list.dirs(path = path, full.names = TRUE, recursive = TRUE)
 
 # calculate data frames
-calculate_datasets(directories)
+#calculate_datasets(directories)
---
+++ analytics/zip-stats-aggregator.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+source("stats-aggregator.R")
+
+#path <- "/Users/kadir/Desktop/ida-gossip/scripts/stats"
+path <- "/Users/kadir/Downloads/experiment-1"
+
+# lists zip files under the folder
+zip_files <- list.files(path = path, pattern = "*.zip", full.names = TRUE)
+
+# unzip all files under /temp_stats_314 folder
+temp_folder <- paste(path, "/temp_stats_314", sep = "")
+for (zip_file in zip_files) {
+  unzip(zipfile = zip_file, exdir = temp_folder)
+}
+
+
+# lists directories
+directories <- list.dirs(paste(temp_folder, "/stats", sep = ""), full.names = TRUE, recursive = TRUE)
+
+# calculate data frames
+calculate_datasets(directories)
+
+# remove temp folder
+# unlink(temp_folder, recursive = TRUE)
+
