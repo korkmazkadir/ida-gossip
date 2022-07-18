9168d9cd73162c8b05722a0477e6a8f41ec0277f
korkmazkadir <korkmazkadir@gmail.com>
2022-07-18 19:42:13 +0200

    Updates create-configs.sh to read chunk count from template-config.json

--- scripts/create-configs.sh
+++ scripts/create-configs.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-message_sizes=(1 2 4 8 12 16 20 24 28 32) #in megabytes
+#message_sizes=(1 2 4 8 12 16 20) #in megabytes
+message_sizes=(20) #in megabytes
 megabyte_in_bytes=1048576 #in bytes
 
 template_config="./template-config.json"
 
+chunk_count=$(cat $template_config | jq ".MessageChunkCount")
+
 file_index=1
 for size in ${message_sizes[@]}; do
 
-    printf -v file_name "%03d_%dMB.json" ${file_index} ${size}
+    printf -v file_name "%03d_%dMB_%dChunks.json" ${file_index} ${size} ${chunk_count}
     echo "${file_name}"
 
     message_size=$(($megabyte_in_bytes * size))
